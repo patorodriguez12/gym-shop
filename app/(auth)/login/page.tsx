a26364ccc7674c413ada799058e2b02481b6ff0c
Hugo Patricio Rodriguez <147743599+patorodriguez12@users.noreply.github.com>
2026-02-16 09:20:37 -0300

    feat: implement login and register pages, not connected to database

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import AuthForm from "../../components/AuthForm";
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  const router = useRouter();
+
+  async function handleLogin(email: string, password: string) {
+    console.log("Login:", email, password);
+
+    await new Promise((res) => setTimeout(res, 1000));
+
+    router.push("/");
+  }
+
+  return (
+    <AuthForm
+      title="Sign in"
+      submitLabel="Login"
+      onSubmit={handleLogin}
+      footer={
+        <>
+          Don’t have an account?{" "}
+          <Link href="/register" className="text-orange-400 hover:underline">
+            Register
+          </Link>
+        </>
+      }
+    />
+  );
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import AuthForm from "../../components/AuthForm";
+
 export default function RegisterPage() {
-  return <div>Register</div>;
+  const router = useRouter();
+
+  async function handleRegister(email: string, password: string) {
+    // aquí irá supabase.signUp()
+    console.log("Register:", email, password);
+
+    await new Promise((res) => setTimeout(res, 1000));
+
+    router.push("/");
+  }
+
+  return (
+    <AuthForm
+      title="Create account"
+      submitLabel="Register"
+      onSubmit={handleRegister}
+      footer={
+        <>
+          Already have an account?{" "}
+          <Link href="/login" className="text-orange-400 hover:underline">
+            Login
+          </Link>
+        </>
+      }
+    />
+  );
 }
